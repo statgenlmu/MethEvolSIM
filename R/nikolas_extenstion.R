9cbadf1b8c34841902d6cb2009c7c43d21720cfa
Nikolas <riccardo.mewis@gmail.com>
2024-07-06 19:01:45 +0200

    changes of nikolas_extension.R with shortening RE_within and returning values if testing==True

--- R/nikolas_extenstion.R
+++ R/nikolas_extenstion.R
@@ -2,8 +2,8 @@ library(R6)
 library(devtools)
 
 
-#get_SrMatrix <- 
-singleStructureGenerator$set("public","get_SrMatrix",function(old_eqFreqs,new_eqFreqs, testing=FALSE){
+#get_SrMatrix <-
+singleStructureGenerator$set("public","get_TransMatrix",function(old_eqFreqs,new_eqFreqs, testing=FALSE){
   #set the transition matrix for the equilibrium frequencies
   Sr <- NULL
   
@@ -115,111 +115,71 @@ singleStructureGenerator$set("public","RE_within",function(Y_seq,Y_eqFreqs,testi
   
   old_eqFreqs <- NULL
   new_eqFreqs <- NULL
+  seq_to_update <- NULL
   
   #if frequencies are identical just concatenate the sequences
   if (identical(X_eqFreqs, Y_eqFreqs)) {
     eqFreqsChange = F
     new_whole_seq <- c(X_seq, Y_seq)
   }
-  
-
-  else if (chosen_eqFreqs == "FreqsX" | chosen_eqFreqs == "FreqsY"){
+  else{
     # determine which frequencies got chosen
     if (chosen_eqFreqs == "FreqsX"){
       #X_eqFreqs is the new frequencies since they get applied to Y as "new" frequencies
       #Y_eqFreqs is the old frequencies since they get replaced by X_eqFreqs
       old_eqFreqs <- Y_eqFreqs
       new_eqFreqs <- X_eqFreqs
-      
-      
-      eqFreqsChange = T
-      #compute the transition matrix for equilibrium frequencies
-      Sr <- self$get_SrMatrix(old_eqFreqs = Y_eqFreqs, new_eqFreqs=X_eqFreqs)
-      # Change data equilibrium frequencies
-      private$eqFreqs <<- X_eqFreqs
-      # Update Qi and Q
-      private$set_Qi()
-      private$set_Q()
-      
-      if(testing){
-        # Validate updated rate matrices
-        validationStates <- listRateMatrix_validation(private$Qi, "Qi matrices RE_within")
-        listMatrices_validationResults(validationStates)
-        
-        validationStates <- listRateMatrix_validation(private$Q[[1]], "Q matrices RE_within R1")
-        listMatrices_validationResults(validationStates)
-        
-        validationStates <- listRateMatrix_validation(private$Q[[2]], "Q matrices RE_within R2")
-        listMatrices_validationResults(validationStates)
-        
-        validationStates <- listRateMatrix_validation(private$Q[[3]], "Q matrices RE_within R3")
-        listMatrices_validationResults(validationStates)
-
-        # Validate methylation equilibrium tripple (extra control at RE_within)
-        validationStates <- listFreqVector_validation(list(old_eqFreqs, new_eqFreqs), "RE_within control old_eqFreqs and new_eqFreqs")
-        listFreqVector_validationResults(validationStates)
-        
-        
-      }
-      
-      # Sample Y_seq according to transition probablities
-      new_Y_seq <- rep(0, length(Y_seq))
-      for(i in 1:length(new_Y_seq)) {
-        new_Y_seq[i] <- sample(1:3, size=1, prob=as.vector(Sr[Y_seq[i],]))
-      }
-      new_whole_seq <- c(X_seq, new_Y_seq)
-      
+      seq_to_update <- Y_seq
     }
-    
+      
     else if (chosen_eqFreqs =="FreqsY"){
-      #the new equilibrium frequencies here are the Y frequencies
-      #the old equilibrium frequencies here are the X frequencies
       old_eqFreqs <- X_eqFreqs
       new_eqFreqs <- Y_eqFreqs
+      seq_to_update <- X_seq
+    }
       
-      eqFreqsChange = T
-      #compute the transition matrix for equilibrium frequencies
-      Sr <- self$get_SrMatrix(old_eqFreqs = X_eqFreqs, new_eqFreqs=Y_eqFreqs)
-      # Change data equilibrium frequencies
-      private$eqFreqs <<- Y_eqFreqs
-      # Update Qi and Q
-      private$set_Qi()
-      private$set_Q()
+    eqFreqsChange = T
+    #compute the transition matrix for equilibrium frequencies
+    Sr <- self$get_TransMatrix(old_eqFreqs = old_eqFreqs, new_eqFreqs=new_eqFreqs)
+    # Change data equilibrium frequencies
+    private$eqFreqs <<- new_eqFreqs
+    # Update Qi and Q
+    private$set_Qi()
+    private$set_Q()
       
-      if(testing){
-        # Validate updated rate matrices
-        validationStates <- listRateMatrix_validation(private$Qi, "Qi matrices RE_within")
-        listMatrices_validationResults(validationStates)
+    if(testing){
+      # Validate updated rate matrices
+      validationStates <- listRateMatrix_validation(private$Qi, "Qi matrices RE_within")
+      listMatrices_validationResults(validationStates)
+    
+      validationStates <- listRateMatrix_validation(private$Q[[1]], "Q matrices RE_within R1")
+      listMatrices_validationResults(validationStates)
         
-        validationStates <- listRateMatrix_validation(private$Q[[1]], "Q matrices RE_within R1")
-        listMatrices_validationResults(validationStates)
+      validationStates <- listRateMatrix_validation(private$Q[[2]], "Q matrices RE_within R2")
+      listMatrices_validationResults(validationStates)
         
-        validationStates <- listRateMatrix_validation(private$Q[[2]], "Q matrices RE_within R2")
-        listMatrices_validationResults(validationStates)
-        
-        validationStates <- listRateMatrix_validation(private$Q[[3]], "Q matrices RE_within R3")
-        listMatrices_validationResults(validationStates)
-        
-        # Validate methylation equilibrium tripple (extra control at RE_within)
-        validationStates <- listFreqVector_validation(list(old_eqFreqs, new_eqFreqs), "RE_within control old_eqFreqs and new_eqFreqs")
-        listFreqVector_validationResults(validationStates)
-        
-      }
+      validationStates <- listRateMatrix_validation(private$Q[[3]], "Q matrices RE_within R3")
+      listMatrices_validationResults(validationStates)
+
+      # Validate methylation equilibrium tripple (extra control at RE_within)
+      validationStates <- listFreqVector_validation(list(old_eqFreqs, new_eqFreqs), "RE_within control old_eqFreqs and new_eqFreqs")
+      listFreqVector_validationResults(validationStates)
       
-      # Sample X_seq according to transition probablities
-      new_X_seq <- rep(0, length(X_seq))
-      for(i in 1:length(new_X_seq)) {
-        new_X_seq[i] <- sample(1:3, size=1, prob=as.vector(Sr[X_seq[i],]))
-      }
-      new_whole_seq <- c(new_X_seq, Y_seq)
+    }
+      
+    # Sample Y_seq according to transition probablities
+    new_seq <- rep(0, length(seq_to_update))
+    for(i in 1:length(new_seq)) {
+      new_seq[i] <- sample(1:3, size=1, prob=as.vector(Sr[seq_to_update[i],]))
+    }
+    if (chosen_eqFreqs=="FreqsX"){
+      new_whole_seq <- c(X_seq, new_seq)
+    }
+    else{
+      new_whole_seq <- c(new_seq, Y_seq)
     }
   }
-  
-  
-  # Update $seq and $neighbSt
-  #print("now:")
-  #print(private$seq)
-  #print(new_whole_seq)
+
   if(any(private$seq != new_whole_seq)){
     changedPos <- which(private$seq != new_whole_seq)
     private$seq <- new_whole_seq
@@ -231,35 +191,20 @@ singleStructureGenerator$set("public","RE_within",function(Y_seq,Y_eqFreqs,testi
       }
     }
   }
-  #if (testing){
-  #  print("Updated sequence:")
-  #  print(private$seq)
-  #  print("New Eq frequencies:")
-  #}
+
 
   #Compute the new_obsFreqs after the RE event
   new_obsFreqs <- c(sum(private$seq==1), sum(private$seq==2), sum(private$seq==3))/length(private$seq)
-  #print(new_obsFreqs)
-  #print(private$seq)
-  
-  if (testing){
-    if(eqFreqsChange){
-      return(list(combiStructure_index = private$combiStructure_index,
-                  eqFreqsChange = eqFreqsChange,
-                  old_eqFreqs = old_eqFreqs,
-                  new_eqFreqs = new_eqFreqs,
-                  old_obsFreqs = old_obsFreqs,
-                  new_obsFreqs = new_obsFreqs,
-                  Sr = Sr,
-                  old_Q = old_Q,
-                  new_Q = private$Q,
-                  changedPos = changedPos))
-    } else {
-      return(list(eqFreqsChange = eqFreqsChange,
-                  old_eqFreqs = old_eqFreqs,
-                  new_eqFreqs = new_eqFreqs))
+  if(testing){
+    if(chosen_eqFreqs=="FreqsX"){
+      return(list(chosenEqFreqs = X_eqFreqs, id="FreqsX"))
+    }
+    else{
+      return(list(chosenEqFreqs = Y_eqFreqs,id= "FreqsY"))
     }
   }
+  
+
 
 })
 
