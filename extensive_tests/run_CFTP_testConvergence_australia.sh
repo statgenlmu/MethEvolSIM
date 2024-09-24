a0258a2c7cf798afecc6577b0d711b3ab9d8ef40
Sara Castillo Vicente <Sara.Castillo@lmu.campus.de>
2024-09-24 10:42:08 +0200

    Updating run_CFTP_testConvergence_australia.sh

--- extensive_tests/run_CFTP_testConvergence_australia.sh
+++ extensive_tests/run_CFTP_testConvergence_australia.sh
@@ -4,11 +4,11 @@
 # First test run: b_length set to 10, start to 1, end to 50
 # Second test run: b_length set to 1, start to 1, end to 500
 
-  Rscript CFTP_testConvergence.R -o /home/sara/methylation/MethEvolSIM/extensive_tests/test2 \
-                                 -f /home/sara/methylation/phd_project_saracv/simulation_studies/abc_designSIM.RData \
+  Rscript CFTP_testConvergence.R -o /scratch/saracv/CFTP_test/test2 \
+                                 -f /scratch/saracv/abc_designSIM.RData \
                                  -b 1 \
                                  -s 1 \
-                                 -e 3 \
+                                 -e 500 \
                                  -p 10 \
                                  -t 2 \
                                  -n CFTP_testConvergence 
