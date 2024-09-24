1094bb67a9c774b07ca59b421c98e3acf6b39b25
Sara Castillo Vicente <Sara.Castillo@lmu.campus.de>
2024-09-24 10:35:33 +0200

    Adding scripts to run test convergence CFTP and updating testConvergence.R

--- extensive_tests/CFTP_testConvergence.R
+++ extensive_tests/CFTP_testConvergence.R
@@ -1,16 +1,51 @@
-# First test run: b_length set to 10, start to 1, end to 50
-# Second test run: b_length set to 1, start to 1, end to 500
+
 
 library(devtools)
 load_all()
 library(parallel)
+library(optparse)
+
+# Define the command-line options
+option_list <- list(
+  make_option(c("-o", "--output-dir"), type = "character", default = NULL,
+              help = "Full path to the output directory", metavar = "character"),
+  make_option(c("-f", "--design-file"), type = "character", default = NULL,
+              help = "Full path to the simulation design file", metavar = "character"),
+  make_option(c("-b", "--branch-length"), type = "integer", default = NULL,
+              help = "length of branch for simulating each evol step", metavar = "integer"),
+  make_option(c("-s", "--start"), type = "integer", default = NULL,
+              help = "start step number for simulating evol", metavar = "integer"),
+  make_option(c("-e", "--end"), type = "integer", default = NULL,
+              help = "end step number for simulating evol", metavar = "integer"),
+  make_option(c("-p", "--paramComb-n"), type = "integer", default = NULL,
+              help = "Number of parameter combinations", metavar = "integer"),
+  make_option(c("-t", "--test-n"), type = "integer", default = NULL,
+              help = "test number ", metavar = "integer"),
+  make_option(c("-n", "--name-pattern"), type = "character", default = NULL,
+              help = "output .RData files start name pattern", metavar = "character")
+)
+
+# Parse the arguments
+
+opt_parser <- OptionParser(option_list = option_list)
+opt <- parse_args(opt_parser)
+
+# Get the names of required options (you can update this based on what's mandatory)
+required_options <- c("output-dir", "design-file", "branch-length", "start", "end", "paramComb-n", "test-n", "name-pattern")
+
+# Check that all required options are not NULL
+missing_options <- required_options[sapply(required_options, function(x) is.null(opt[[x]]))]
+
+if (length(missing_options) > 0) {
+  print_help(opt_parser)
+  stop(paste("The following arguments need to be provided:", paste(missing_options, collapse = ", ")))
+}
 
 
-
-simul_CFTP_branch <- function(custom_params, index_params, b_length, start, end, out_digit_n, spatial_str){
+simul_CFTP_branch <- function(custom_params, index_params, b_length, start, end, out_digit_n, spatial_str, test_n){
   # Set the name for the output file with the padded parameter index
   padded_index_params <- formatC(index_params, width = 2, format = "d", flag = "0")
-  out_file <- paste0("/scratch/saracv/CFTP_test/CFTP_testConvergence2_paramsID_", padded_index_params, ".out")
+  out_file <- paste0(opt[["output-dir"]], "/", opt[["name-pattern"]], test_n, "_paramsID_", padded_index_params, ".out")
   # Redirect both the stout and stderr to the same file
   sink(out_file, type = c("output", "message"), append = TRUE)
   print(paste("Running CFTP_testConvergence2_paramsID", padded_index_params))
@@ -19,6 +54,14 @@ simul_CFTP_branch <- function(custom_params, index_params, b_length, start, end,
   if(start == 1){
     print("Generating combiStructureGenerator instance")
     combi <- combiStructureGenerator$new(infoStr = spatial_str, params = custom_params)
+    # Save initial instance state and methylation data
+    data <- list()
+    for (str in 1:combi$get_singleStr_number()){
+      data[[str]]<- transform_methStateEncoding(combi$get_singleStr(str)$get_seq())
+    }
+    padded_sim_n <- formatC(0, width = out_digit_n, format = "d", flag = "0")
+    RData_file <- paste0(opt[["output-dir"]], "/", opt[["name-pattern"]], test_n, "_paramsID_", padded_index_params, "_", padded_sim_n, ".RData")
+    save(data, combi, file = RData_file)
   }
   # Simulate evolution along branch n times
   print(paste("Simulating evolution along branch of length", b_length, end - start + 1, "times."))
@@ -33,11 +76,12 @@ simul_CFTP_branch <- function(custom_params, index_params, b_length, start, end,
     }
     # Save simulated data
     padded_sim_n <- formatC(i, width = out_digit_n, format = "d", flag = "0")
+    RData_file <- paste0(opt[["output-dir"]], "/", opt[["name-pattern"]], test_n, "_paramsID_", padded_index_params, "_", padded_sim_n, ".RData")
     if (i == end){
       # The last time, save also the combiStructureGenerator instance, to be able to start new simulations from last state
-      save(data, combi, file = paste0("/scratch/saracv/CFTP_test/CFTP_testConvergence2_paramsID_", padded_index_params, "_n_", padded_sim_n, ".RData" ))
+      save(data, combi, file = RData_file)
     } else {
-      save(data, file = paste0("/scratch/saracv/CFTP_test/CFTP_testConvergence2_paramsID_", padded_index_params, "_n_", padded_sim_n, ".RData" ))
+      save(data, file = RData_file)
     }
   }
   # Stop redirecting output and messages
@@ -45,14 +89,9 @@ simul_CFTP_branch <- function(custom_params, index_params, b_length, start, end,
 }
 
 simul_CFTP_tests <- function(index_params){
-  # Set branch length
-  b_length <- 1
-  # Set start and end for the number of times to conduct simulations along the branch
-  start <- 1
-  end <- 500
   out_digit_n <- 4
   # Load the combinations of sampled parameters and spatial structure
-  load("/scratch/saracv/abc_designSIM.RData")
+  load(opt[["design-file"]])
   # Set parameter combination case
   custom_params <- sampled_params[index_params,]
   # Set IWE rate to 0
@@ -60,18 +99,17 @@ simul_CFTP_tests <- function(index_params){
   # Call function to simulate data
   simul_CFTP_branch(custom_params = custom_params,
                     index_params = index_params,
-                    b_length = b_length,
-                    start = start,
-                    end = end,
-                    out_digit_n = out_digit_n,
-                    spatial_str = spatial_str)
+                    b_length = opt[["branch-length"]], # Set branch length
+                    start = opt[["start"]], # Set start and end for the number of times to conduct simulations along the branch
+                    end = opt[["end"]],
+                    out_digit_n = 4, # Set number for maximum width of padded simulation number 
+                    spatial_str = spatial_str,
+                    test_n = opt[["test-n"]])
   
 }
 
 # Run in parallel using mclapply
-paramComb_n <- 10
-mclapply(1:paramComb_n, function(index_params) simul_CFTP_tests(index_params), mc.cores = 10)
-
+mclapply(1:opt[["paramComb-n"]], function(index_params) simul_CFTP_tests(index_params), mc.cores = opt[["paramComb-n"]])
 
 
 
