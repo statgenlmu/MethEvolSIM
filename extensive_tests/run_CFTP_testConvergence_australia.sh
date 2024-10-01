a7b80e22a2e6c36250e8b9f5bc523ff5bdb1f247
Sara Castillo Vicente <Sara.Castillo@lmu.campus.de>
2024-10-01 11:30:38 +0200

    Updating run_CFTP_testConvergence_australia.sh

--- extensive_tests/run_CFTP_testConvergence_australia.sh
+++ extensive_tests/run_CFTP_testConvergence_australia.sh
@@ -2,13 +2,13 @@
 #SBATCH --job-name=CFTP_testConvergence_local
 
 # First test run: b_length set to 10, start to 1, end to 50
-# Second test run: b_length set to 1, start to 1, end to 500
+# Second test run: b_length set to 0.1, start to 1, end to 500
 
   Rscript CFTP_testConvergence.R -o /scratch/saracv/CFTP_test/test2 \
                                  -f /scratch/saracv/abc_designSIM.RData \
-                                 -b 1 \
+                                 -b 0.1 \
                                  -s 1 \
-                                 -e 250 \
+                                 -e 500 \
                                  -p 10 \
                                  -t 2 \
                                  -n CFTP_testConvergence \
