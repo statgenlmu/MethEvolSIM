944a02a5d2db41fb3f0bbab28b9083b74c52c044
Sara Castillo Vicente <Sara.Castillo@lmu.campus.de>
2024-10-02 13:05:22 +0200

    Updated run_compute_summaryStats_CFTP_australia.sh to compute summary stats separatedly for output of branch_evol and cftp methods

--- extensive_tests/run_compute_summaryStats_CFTP_australia.sh
+++ extensive_tests/run_compute_summaryStats_CFTP_australia.sh
@@ -10,12 +10,19 @@ do
     param_id="nestedReps_CFTP_testConvergence2_paramsID_$i"
   fi
     
-    # Call the R script with the new rep_id
+    # Call the R script for $branch_evol output data
     Rscript compute_summaryStats.R -d /scratch/saracv/CFTP_test/test2 \
                                    -p $param_id \
                                    -f /scratch/saracv/abc_designSIM.RData \
                                    -n 10 \
-                                   -s meanFreqP_i,meanFreqP_ni,sdFreqP_i,sdFreqP_ni,meanFreqM_i,meanFreqM_ni,sdFreqM_i,sdFreqM_ni,meanCor,Steepness	
+                                   -s meanFreqP_i,meanFreqP_ni,sdFreqP_i,sdFreqP_ni,meanFreqM_i,meanFreqM_ni,sdFreqM_i,sdFreqM_ni,meanCor,Steepness
+                                   
+    # Call the R script for $cftp output data
+    Rscript compute_summaryStats_cftpOutput.R -d /scratch/saracv/CFTP_test/test2 \
+    					      -p $param_id \
+    					      -f /scratch/saracv/abc_designSIM.RData \
+    					      -n 10 \
+    					      -s meanFreqP_i,meanFreqP_ni,sdFreqP_i,sdFreqP_ni,meanFreqM_i,meanFreqM_ni,sdFreqM_i,sdFreqM_ni,meanCor,Steepness	
 done
 
 
