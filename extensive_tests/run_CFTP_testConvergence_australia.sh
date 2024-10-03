227aba5ff124860f1e4b97c8bc5dd2999b71e866
Sara Castillo Vicente <Sara.Castillo@lmu.campus.de>
2024-10-03 10:24:32 +0200

    Updating run_CFTP_testConvergence_australia.sh for test4 (debugging)

--- extensive_tests/run_CFTP_testConvergence_australia.sh
+++ extensive_tests/run_CFTP_testConvergence_australia.sh
@@ -3,15 +3,16 @@
 
 # First test run: b_length set to 10, start to 1, end to 50
 # Second test run: b_length set to 0.1, start to 1, end to 500. 
-# Debug run (test 3): commented out method $cftp, only 3 params
+# Debug run (test 3): commented out method $cftp. Leads to no changes
+# Debut run (test 4): b_length set to 1, start to 1, end to 250
 
-  Rscript CFTP_testConvergence.R -o /scratch/saracv/CFTP_test/test3 \
+  Rscript CFTP_testConvergence.R -o /scratch/saracv/CFTP_test/test4 \
                                  -f /scratch/saracv/abc_designSIM.RData \
-                                 -b 0.1 \
+                                 -b 1 \
                                  -s 1 \
-                                 -e 500 \
+                                 -e 250 \
                                  -p 10 \
-                                 -t 3 \
+                                 -t 4 \
                                  -n CFTP_testConvergence \
                                  -r 10
                                  
