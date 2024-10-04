15e027a1a0eea51d064adc692bf6a5139a272e48
Sara Castillo Vicente <Sara.Castillo@lmu.campus.de>
2024-10-04 07:13:54 +0200

    Modifying CFTP_testConvergence.R to take argument branch as numeric instead of integer and run_CFTP_testConvergence_australia to run test6

--- extensive_tests/run_CFTP_testConvergence_australia.sh
+++ extensive_tests/run_CFTP_testConvergence_australia.sh
@@ -6,14 +6,15 @@
 # Debug run (test 3): commented out method $cftp. Leads to no changes
 # Debut run (test 4): b_length set to 1, start to 1, end to 250
 # Debug run (test 5): keeps previous, but uncomments $cftp
+# Debug run (test 6): Modified -b in script from integer to numeric. b_length set to 0.1, start to 1, end to 500.
 
-  Rscript CFTP_testConvergence.R -o /scratch/saracv/CFTP_test/test5 \
+  Rscript CFTP_testConvergence.R -o /scratch/saracv/CFTP_test/test6 \
                                  -f /scratch/saracv/abc_designSIM.RData \
-                                 -b 1 \
+                                 -b 0.1 \
                                  -s 1 \
-                                 -e 250 \
+                                 -e 500 \
                                  -p 10 \
-                                 -t 5 \
+                                 -t 6 \
                                  -n CFTP_testConvergence \
                                  -r 10
                                  
