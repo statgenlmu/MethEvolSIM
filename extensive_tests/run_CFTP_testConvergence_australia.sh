3ff71106c1e338df2f074ca71e9a34fb34891651
Sara Castillo Vicente <Sara.Castillo@lmu.campus.de>
2024-10-11 09:39:49 +0200

    Adding extensive_tests/run_CFTP_testConvergence_australia.sh

--- extensive_tests/run_CFTP_testConvergence_australia.sh
+++ extensive_tests/run_CFTP_testConvergence_australia.sh
@@ -5,16 +5,16 @@
 # Second test run: b_length set to 1, start to 1, end to 250.
 
 # Define the test number variable
-test_n=1
+test_n=2
 
 # Define the base directory path
 base_dir="/scratch/saracv/CFTP_test/test${test_n}"
 
   Rscript CFTP_testConvergence.R -o $base_dir\
                                  -f /scratch/saracv/abc_designSIM.RData \
-                                 -b 0.1 \
+                                 -b 1 \
                                  -s 1 \
-                                 -e 500 \
+                                 -e 250 \
                                  -p 10 \
                                  -t $test_n \
                                  -n CFTP_testConvergence \
