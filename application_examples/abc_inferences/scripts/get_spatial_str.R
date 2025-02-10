dcacf6fa585e5aa1a917b554035f9f6d3edb45f6
Sara Castillo Vicente <Sara.Castillo@lmu.campus.de>
2025-02-10 10:13:06 +0100

    Added rule to filter GpG counts relative to the genomic region of interest

--- application_examples/abc_inferences/scripts/get_spatial_str.R
+++ application_examples/abc_inferences/scripts/get_spatial_str.R
@@ -4,17 +4,7 @@ option_list <- list(
   make_option(c("--input"), type = "character", default = NULL,
               help = "Input CpG_count.RData path", metavar = "character"),
   make_option(c("--output"), type = "character", default = NULL,
-              help = "Output full path with file name", metavar = "character"),
-  make_option(c("--name-selected-region"), type = "character", default = NULL,
-              help = "Name of selected genomic region", metavar = "character"),
-  make_option(c("--chr"), type = "character", default = NULL,
-              help = "Chromosome ID of selected region", metavar = "character"),
-  make_option(c("--start"), type = "integer", default = NULL,
-              help = "Start index of selected genomic region", metavar = "integer"),
-  make_option(c("--end"), type = "integer", default = NULL,
-              help = "End index of selected genomic region", metavar = "integer"),
-  make_option(c("--side-bp"), type = "integer", default = NULL,
-              help = "Number of bp to select structures starting after start - side-bp and ending before end + side-bp", metavar = "integer")
+              help = "Output full path with file name", metavar = "character")
 )
 
 # Parse the arguments
@@ -23,7 +13,7 @@ opt_parser <- OptionParser(option_list = option_list)
 opt <- parse_args(opt_parser)
 
 # Get the names of required options (you can update this based on what's mandatory)
-required_options <- c("input", "output","name-selected-region", "chr", "start", "end", "side-bp")
+required_options <- c("input", "output")
 
 # Check that all required options are not NULL
 missing_options <- required_options[sapply(required_options, function(x) is.null(opt[[x]]))]
@@ -36,9 +26,6 @@ if (length(missing_options) > 0) {
 # Load CpG counts
 load(opt[["input"]])
 
-# Filter the data for the given chromosome and region
-CpG_count_region <- CpG_count[CpG_count$chr == opt[["chr"]] & CpG_count$start >= opt[["start"]] - opt[["side-bp"]] & CpG_count$start <= opt[["end"]] + opt[["side-bp"]], ]
-
 # Create the data frame spatial_str
 spatial_str <- data.frame(n = CpG_count_region$CpG_count)
 
@@ -46,9 +33,4 @@ spatial_str <- data.frame(n = CpG_count_region$CpG_count)
 spatial_str$globalState <- ifelse(CpG_count_region$str == "Island", "U", "M")
 
 # Save spatial structure
-#output_name <- paste0(paste(opt[["chr"]], opt[["start"]] - opt[["side-bp"]], opt[["end"]] + opt[["side-bp"]], opt[["name-selected-region"]], sep = "_"), ".RData")
 save(spatial_str, file = opt[["output"]])
-
-##TODO: save CpG_count_region as region_CGI-Mzebra.RData. 
-# Maybe rename it as region_cgi. Then update name it in get_CpG_positions
-# or if in next script region_CpG_positions.
